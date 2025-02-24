47493b0bb61f3483e17679a26f343dc8b323e0b4
Petrichor2390 <cfawcett2002@gmail.com>
2025-02-24 21:32:58 +1100

    removed build folder

--- ros2_rs2_packages/ur3_test_control/build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/charles/ros2_ws/src/ur3_test_control
-# Build directory: /home/charles/ros2_ws/src/ur3_test_control/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
